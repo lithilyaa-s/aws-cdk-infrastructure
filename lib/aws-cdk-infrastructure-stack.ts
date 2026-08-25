eef421ccfc000f60a8aaa697f31282adc695b914
LITHILYAA S <YOUR_GITHUB_EMAIL>
2026-08-25 19:00:49 +0530

    Implement AWS infrastructure with CDK

--- lib/aws-cdk-infrastructure-stack.ts
+++ lib/aws-cdk-infrastructure-stack.ts
@@ -1,16 +1,70 @@
 import * as cdk from 'aws-cdk-lib/core';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as s3 from 'aws-cdk-lib/aws-s3';
 
 export class AwsCdkInfrastructureStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // VPC
+    const vpc = new ec2.Vpc(this, 'ProjectVpc', {
+      maxAzs: 2,
+      natGateways: 0,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsCdkInfrastructureQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
-}
+    // Security Group
+    const securityGroup = new ec2.SecurityGroup(this, 'ProjectSecurityGroup', {
+      vpc,
+      description: 'Security group for the EC2 instance',
+      allowAllOutbound: true,
+    });
+
+    // Allow SSH access
+    securityGroup.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(22),
+      'Allow SSH access'
+    );
+
+    // EC2 Instance
+    const instance = new ec2.Instance(this, 'ProjectEC2Instance', {
+      vpc,
+      instanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.T2,
+        ec2.InstanceSize.MICRO
+      ),
+      machineImage: ec2.MachineImage.latestAmazonLinux2023(),
+      securityGroup,
+    });
+
+    // S3 Bucket
+    const bucket = new s3.Bucket(this, 'ProjectS3Bucket', {
+      versioned: true,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      encryption: s3.BucketEncryption.S3_MANAGED,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    });
+
+    // CloudFormation Outputs
+    new cdk.CfnOutput(this, 'VpcId', {
+      value: vpc.vpcId,
+      description: 'VPC ID',
+    });
+
+    new cdk.CfnOutput(this, 'EC2InstanceId', {
+      value: instance.instanceId,
+      description: 'EC2 Instance ID',
+    });
+
+    new cdk.CfnOutput(this, 'S3BucketName', {
+      value: bucket.bucketName,
+      description: 'S3 Bucket Name',
+    });
+
+    new cdk.CfnOutput(this, 'SecurityGroupId', {
+      value: securityGroup.securityGroupId,
+      description: 'Security Group ID',
+    });
+  
